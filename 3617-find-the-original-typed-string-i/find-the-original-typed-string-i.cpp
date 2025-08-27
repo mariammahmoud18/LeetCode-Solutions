2139dc4d996497d6aa075a360c7ea7d4a68b16f3
Mariam Swelam <62444533+mariammahmoud18@users.noreply.github.com>
2025-08-27 23:41:09 +0300

    Time: 5 ms (23.83%) | Memory: 8.8 MB (64.20%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     int possibleStringCount(string word) {
         int sum = 0;
-        map<char,int>mp;
-        /*for(char c : word){
-            mp[c]++;
-        }
-        for(auto it : mp){
-            sum+=(it.second - 1);
-        }*/
 
         for(int i = 0; i < word.length(); i++){
             if(word[i] == word[i+1]){
