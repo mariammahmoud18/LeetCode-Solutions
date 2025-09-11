b5c1c784d6d5581503e2029af68df0b52aeb15c5
Mariam Swelam <62444533+mariammahmoud18@users.noreply.github.com>
2025-09-11 18:31:09 +0300

    Time: 1197 ms (96.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT s.name
+FROM SalesPerson AS s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders AS o
+    INNER JOIN Company AS c
+      ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
