d8089efb51bd4c3d66cade75f4cd3b6344f9702b
Mariam Swelam <62444533+mariammahmoud18@users.noreply.github.com>
2025-11-11 00:13:47 +0200

    Time: 0 ms (100.00%) | Memory: 10.2 MB (40.27%) - LeetSync

--- 837-most-common-word/most-common-word.cpp
+++ 837-most-common-word/most-common-word.cpp
@@ -8,30 +8,25 @@ public:
         for (int i = 0; i < paragraph.length(); i++) {
             if (paragraph[i] == ' ' || paragraph[i] == ',' ||
                 paragraph[i] == '.' || paragraph[i] == '!' ||
-                paragraph[i] == '?' || paragraph[i] == ';' || paragraph[i] == '\'') {
-                    if(temp == "" || temp == " ")
-                        continue;
-                    cout<<"hmmm: "<<temp<<endl;
+                paragraph[i] == '?' || paragraph[i] == ';' ||
+                paragraph[i] == '\'') {
+                if (temp == "" || temp == " ")
+                    continue;
 
                 mp[temp]++;
                 temp = "";
             } else
                 temp += paragraph[i];
         }
-        if (temp != "" && temp != " " && temp != "," && temp != "." && temp != "!" &&
-            temp != "?" && temp != ":") {
+        if (temp != "" && temp != " " && temp != "," && temp != "." &&
+            temp != "!" && temp != "?" && temp != ":") {
             mp[temp]++;
-            cout << "inside" << endl;
         }
-        cout << temp << endl;
         int maxi = -1;
         string ans = "";
         for (auto it : mp) {
-            cout << "here: " << it.first << endl;
-            cout<<(it.second > maxi);
             if (it.second > maxi && std::find(banned.begin(), banned.end(),
                                               it.first) == banned.end()) {
-                                                cout<<"weeee"<<endl;
                 maxi = it.second;
                 ans = it.first;
             }
